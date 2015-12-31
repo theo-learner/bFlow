d12e1d1348ff6164a564ab8c29043ee5ac358d67
zkaiwen <zkaiwen@gmail.com>
2015-12-30 23:34:51 -0500

    Removed Database circuits from GitHub. Does not need revision

--- v/altera_unsigned_mult.v
+++
@@ -1,8 +0,0 @@
-module altera_unsigned_mult (out, a, b);
-output [15:0] out;
-	input  [7:0] a;
-	input  [7:0] b;
-
-	assign out = a * b;
-
-endmodule
--- v/countOnes.v
+++
@@ -1,7 +0,0 @@
-module countOnes(input [7:0] register, input [3:0] numOnes);
-
-		assign count = register[0] + register[1] + register[2] + register[3] + register[4] + register[5] + register[6] + register[7];
-		
-endmodule
-
-
